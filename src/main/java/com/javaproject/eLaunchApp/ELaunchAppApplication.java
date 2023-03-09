af476ffac2fb00284d2ef80a97fe33643342feaa
Mateusz Orczewski <m.orczewski@gmail.com>
2023-03-09 18:54:21 +0100

    feature: base project with dependencies

---
+++ src/main/java/com/javaproject/eLaunchApp/ELaunchAppApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.eLaunchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELaunchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELaunchAppApplication.class, args);
+	}
+
+}
