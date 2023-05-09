fa95b5140d9c2d41c241c05b72b0951008db0c1d
Mateusz Orczewski <m.orczewski@gmail.com>
2023-05-09 18:10:35 +0200

    feature: add class IngredientController

---
+++ src/main/java/com/javaproject/eLaunchApp/controller/IngredientController.java
@@ -0,0 +1,41 @@
+package com.javaproject.eLaunchApp.controller;
+
+import com.javaproject.eLaunchApp.DTO.DelivererDTO;
+import com.javaproject.eLaunchApp.DTO.IngredientDTO;
+import com.javaproject.eLaunchApp.service.DelivererService;
+import com.javaproject.eLaunchApp.service.IngredientService;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "/api/dingredients", produces = MediaType.APPLICATION_JSON_VALUE)
+public class IngredientController {
+
+    private final IngredientService ingredientService;
+
+    @GetMapping
+    public List<IngredientDTO> get() {
+        return null;
+    }
+
+    @GetMapping("/{uuid}")
+    public IngredientDTO get(@PathVariable UUID uuid) {
+        return null;
+    }
+
+    @Transactional
+    @PutMapping("/{uuid}")
+    public  void put(@PathVariable UUID uuid, @RequestBody IngredientDTO ingredientDTO) {
+        return;
+    }
+
+    @Transactional
+    @DeleteMapping("/{uuid}")
+    public  void delete(@PathVariable UUID uuid) {
+        return;
+    }
+}
