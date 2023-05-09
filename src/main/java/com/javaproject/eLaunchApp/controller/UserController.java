b8143c3b7a4ffa06f3fa9708fad08a388d7f4f6b
Mateusz Orczewski <m.orczewski@gmail.com>
2023-05-09 18:19:23 +0200

    feature: add class UserController

---
+++ src/main/java/com/javaproject/eLaunchApp/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.javaproject.eLaunchApp.controller;
+
+import com.javaproject.eLaunchApp.DTO.DelivererDTO;
+import com.javaproject.eLaunchApp.DTO.UserDTO;
+import com.javaproject.eLaunchApp.service.DelivererService;
+import com.javaproject.eLaunchApp.service.UserService;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "/api/users", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserDTO> get() {
+        return null;
+    }
+
+    @GetMapping("/{uuid}")
+    public UserDTO get(@PathVariable UUID uuid) {
+        return null;
+    }
+
+    @Transactional
+    @PutMapping("/{uuid}")
+    public  void put(@PathVariable UUID uuid, @RequestBody UserDTO userDTO) {
+        return;
+    }
+
+    @Transactional
+    @DeleteMapping("/{uuid}")
+    public  void delete(@PathVariable UUID uuid) {
+        return;
+    }
+}
