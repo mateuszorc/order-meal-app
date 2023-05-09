efd6b0a27044cba7811d29fd2f3efcb4b1bb558b
Mateusz Orczewski <m.orczewski@gmail.com>
2023-05-09 18:14:37 +0200

    feature: add class OpenTimeController

---
+++ src/main/java/com/javaproject/eLaunchApp/controller/OpenTimeController.java
@@ -0,0 +1,41 @@
+package com.javaproject.eLaunchApp.controller;
+
+import com.javaproject.eLaunchApp.DTO.DelivererDTO;
+import com.javaproject.eLaunchApp.DTO.OpenTimeDTO;
+import com.javaproject.eLaunchApp.service.DelivererService;
+import com.javaproject.eLaunchApp.service.OpenTimeService;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "/api/open-times", produces = MediaType.APPLICATION_JSON_VALUE)
+public class OpenTimeController {
+
+    private final OpenTimeService openTimeService;
+
+    @GetMapping
+    public List<OpenTimeDTO> get() {
+        return null;
+    }
+
+    @GetMapping("/{uuid}")
+    public OpenTimeDTO get(@PathVariable UUID uuid) {
+        return null;
+    }
+
+    @Transactional
+    @PutMapping("/{uuid}")
+    public  void put(@PathVariable UUID uuid, @RequestBody OpenTimeDTO openTimeDTO) {
+        return;
+    }
+
+    @Transactional
+    @DeleteMapping("/{uuid}")
+    public  void delete(@PathVariable UUID uuid) {
+        return;
+    }
+}
