fa08cf4fdf31716db6a0721f69cf54dbced86c12
Mateusz Orczewski <m.orczewski@gmail.com>
2023-05-09 18:12:58 +0200

    feature: add class MenuItemController

---
+++ src/main/java/com/javaproject/eLaunchApp/controller/MenuItemController.java
@@ -0,0 +1,41 @@
+package com.javaproject.eLaunchApp.controller;
+
+import com.javaproject.eLaunchApp.DTO.DelivererDTO;
+import com.javaproject.eLaunchApp.DTO.MenuItemDTO;
+import com.javaproject.eLaunchApp.service.DelivererService;
+import com.javaproject.eLaunchApp.service.MenuItemService;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "/api/menu-items", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MenuItemController {
+
+    private final MenuItemService menuItemService;
+
+    @GetMapping
+    public List<MenuItemDTO> get() {
+        return null;
+    }
+
+    @GetMapping("/{uuid}")
+    public MenuItemDTO get(@PathVariable UUID uuid) {
+        return null;
+    }
+
+    @Transactional
+    @PutMapping("/{uuid}")
+    public  void put(@PathVariable UUID uuid, @RequestBody MenuItemDTO menuItemDTO) {
+        return;
+    }
+
+    @Transactional
+    @DeleteMapping("/{uuid}")
+    public  void delete(@PathVariable UUID uuid) {
+        return;
+    }
+}
