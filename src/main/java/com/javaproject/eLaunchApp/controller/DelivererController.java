34d2797dfce277b00765e9fdeade8a8e240c4ff0
Mateusz Orczewski <m.orczewski@gmail.com>
2023-05-09 18:01:54 +0200

    feature: add class DelivererController

---
+++ src/main/java/com/javaproject/eLaunchApp/controller/DelivererController.java
@@ -0,0 +1,39 @@
+package com.javaproject.eLaunchApp.controller;
+
+import com.javaproject.eLaunchApp.DTO.DelivererDTO;
+import com.javaproject.eLaunchApp.service.DelivererService;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "/api/deliverers", produces = MediaType.APPLICATION_JSON_VALUE)
+public class DelivererController {
+
+    private final DelivererService delivererService;
+
+    @GetMapping
+    public List<DelivererDTO> get() {
+        return null;
+    }
+
+    @GetMapping("/{uuid}")
+    public DelivererDTO get(@PathVariable UUID uuid) {
+        return null;
+    }
+
+    @Transactional
+    @PutMapping("/{uuid}")
+    public  void put(@PathVariable UUID uuid, @RequestBody DelivererDTO delivererDTO) {
+        return;
+    }
+
+    @Transactional
+    @DeleteMapping("/{uuid}")
+    public  void delete(@PathVariable UUID uuid) {
+        return;
+    }
+}
