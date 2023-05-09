17d0b9d8ea9775bbda6741c30f8b2112aa14551b
Mateusz Orczewski <m.orczewski@gmail.com>
2023-05-09 18:17:38 +0200

    feature: add class RestaurantController

---
+++ src/main/java/com/javaproject/eLaunchApp/controller/RestaurantController.java
@@ -0,0 +1,41 @@
+package com.javaproject.eLaunchApp.controller;
+
+import com.javaproject.eLaunchApp.DTO.DelivererDTO;
+import com.javaproject.eLaunchApp.DTO.RestaurantDTO;
+import com.javaproject.eLaunchApp.service.DelivererService;
+import com.javaproject.eLaunchApp.service.RestaurantService;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "/api/restaurants", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    @GetMapping
+    public List<RestaurantDTO> get() {
+        return null;
+    }
+
+    @GetMapping("/{uuid}")
+    public RestaurantDTO get(@PathVariable UUID uuid) {
+        return null;
+    }
+
+    @Transactional
+    @PutMapping("/{uuid}")
+    public  void put(@PathVariable UUID uuid, @RequestBody RestaurantDTO restaurantDTO) {
+        return;
+    }
+
+    @Transactional
+    @DeleteMapping("/{uuid}")
+    public  void delete(@PathVariable UUID uuid) {
+        return;
+    }
+}
